3039effa1a15da7f2235173c64117c1ee6990c02
Nikita <nikitakubavat33@gmail.com>
2023-05-08 11:53:12 +0530

    Done: Profile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import Projects from "./pages/Projects";
 import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div>
-      <Contact />
+      <Profile />
     </div>
   );
 }
